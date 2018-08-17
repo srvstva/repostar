664e08c8748df3c84a8f46df510d77a7a8738abd
Ankur Srivastava <srvstva@outlook.com>
2018-08-17 12:43:42 +0530

    Updated config with secret key

--- repostar/config.py
+++ repostar/config.py
@@ -3,5 +3,6 @@ import os
 
 ENV = 'development'
 DEBUG = True
+SECRET_KEY = '54e7fdf6cd7c76cd1235bb3c98f6b083'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.dirname(os.path.abspath(__name__)) + '/development.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
